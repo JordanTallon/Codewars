5ee76521378d041a6fb25d437b9463e60c9a4fde
JordanTallon <60022738+JordanTallon@users.noreply.github.com>
2020-01-18 06:42:26 +0000

    Update Valid Parentheses.cs

--- Novice/Valid Parentheses.cs
+++ Novice/Valid Parentheses.cs
@@ -3,22 +3,23 @@
    if the order of the parentheses is valid. The function should return true if the string is valid,
    and false if it's invalid.*/
   
-  public class Parentheses
+public class Parentheses
+{
+    public static bool ValidParentheses(string input)
     {
-        public static bool ValidParentheses(string input)
+        while(input.Contains('(') && input.Contains(')'))
         {
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (input[i] != '(' && input[i] != ')') { continue; }
-                bool closed = false;
-                if (input[i] == '(') { for (int h = i; h < (input.Length); h++) { if (input[h] == ')') { closed = true; } } }
-                if (input[i] == ')') { for (int h = i; h > -1; h--) { if (input[h] == '(') { closed = true; } } }
-                if (!closed){return false;}
-            }
-            return true;
+          int index = input.IndexOf('(');
+          bool closed = false;
+          for(int i = index; i < input.Length;i++)
+          {
+            if(input[i] == ')'){input = input.Remove(i,1); input = input.Remove(index,1);closed = true;break;}
+          }
+          if(!closed){return false;}
         }
+        return !(input.Contains('(') || input.Contains(')')); 
     }
-    
+}
 /*Output Examples
 "(z)"              =>  true
 ")((x)))"          =>  false
