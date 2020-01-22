dddd97ef77f5ecf064a85ef0471d81babcb2bfab
JordanTallon <60022738+JordanTallon@users.noreply.github.com>
2020-01-22 11:06:13 +0000

    Update Permutations.cs

--- Competent/Permutations.cs
+++ Competent/Permutations.cs
@@ -1,6 +1,6 @@
 /*Challenge:
 In this kata you have to create all permutations of an input string and remove duplicates, if present. 
-This means, you have to shuffle all letters from the input in all possible orders.
+This means, you have to shuffle all letters from the input in all possible orders.*/
 
 using System;
 using System.Collections.Generic;
